e08d5f42f9ae85c07e80f820ac034113d7379473
unknown <Luis15Herr>
2022-02-02 18:14:45 -0400

    html fixes

--- js.js
+++ js.js
@@ -46,10 +46,10 @@ function buildComments() {
     html = `<div class="comment" data-id="${item.id}">
     <div class="comment__wrapper wrapper" >
     <div class="comment__score">
-    <button class='comment__score-icon comment__score-item upvote' src="./images/icon-plus.svg" alt="PLUS BTN">
+    <button class='comment__score-icon comment__score-item upvote'>
     <svg width="11" height="11" xmlns="http://www.w3.org/2000/svg"><path d="M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z" fill="#C5C6EF"/></svg>
     </button> <span class='comment__score-number'>${item.score}</span> 
-    <button class='comment__score-icon comment__score-item downvote' src="./images/icon-minus.svg" alt="MINUS BTN">
+    <button class='comment__score-icon comment__score-item downvote'>
     <svg width="11" height="3" xmlns="http://www.w3.org/2000/svg"><path d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z" fill="#C5C6EF"/></svg>
     </button>
     </div>
@@ -99,10 +99,10 @@ function buildComments() {
         replies = `<div class="reply" data-id="${reply.id}">
         <div class="reply__wrapper wrapper">
         <div class="comment__score">
-        <button class='comment__score-icon comment__score-item upvote' src="./images/icon-plus.svg" alt="PLUS BTN">
+        <button class='comment__score-icon comment__score-item upvote'>
         <svg width="11" height="11" xmlns="http://www.w3.org/2000/svg"><path d="M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z" fill="#C5C6EF"/></svg>
         </button> <span class='comment__score-number'>${reply.score}</span> 
-        <button class='comment__score-icon comment__score-item downvote' src="./images/icon-minus.svg" alt="MINUS BTN">
+        <button class='comment__score-icon comment__score-item downvote'>
         <svg width="11" height="3" xmlns="http://www.w3.org/2000/svg"><path d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z" fill="#C5C6EF"/></svg>
         </button>
         </div>
@@ -137,10 +137,10 @@ function buildReply() {
   replies = `<div class="reply new-reply" data-id="${reply.id}">
   <div class="reply__wrapper wrapper">
   <div class="comment__score">
-  <button class='comment__score-icon comment__score-item upvote' src="./images/icon-plus.svg" alt="PLUS BTN">
+  <button class='comment__score-icon comment__score-item upvote' >
   <svg width="11" height="11" xmlns="http://www.w3.org/2000/svg"><path d="M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z" fill="#C5C6EF"/></svg>
   </button> <span class='comment__score-number'>${reply.score}</span> 
-  <button class='comment__score-icon comment__score-item downvote' src="./images/icon-minus.svg" alt="MINUS BTN">
+  <button class='comment__score-icon comment__score-item downvote'>
   <svg width="11" height="3" xmlns="http://www.w3.org/2000/svg"><path d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z" fill="#C5C6EF"/></svg>
   </button>
   </div>
@@ -178,10 +178,10 @@ function buildComment() {
   html = `<div class="comment" data-id="${comment.id}">
   <div class="comment__wrapper wrapper">
   <div class="comment__score">
-  <button class='comment__score-icon comment__score-item upvote' src="./images/icon-plus.svg" alt="PLUS BTN">
+  <button class='comment__score-icon comment__score-item upvote>
   <svg width="11" height="11" xmlns="http://www.w3.org/2000/svg"><path d="M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z" fill="#C5C6EF"/></svg>
   </button> <span class='comment__score-number'>${comment.score}</span> 
-  <button class='comment__score-icon comment__score-item downvote' src="./images/icon-minus.svg" alt="MINUS BTN">
+  <button class='comment__score-icon comment__score-item downvote'>
   <svg width="11" height="3" xmlns="http://www.w3.org/2000/svg"><path d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z" fill="#C5C6EF"/></svg>
   </button>
   </div>
